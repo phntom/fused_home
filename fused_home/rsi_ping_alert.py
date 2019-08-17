c6241fd4c5f7024683b94f8507bce27d1c43cb1e
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2019-08-17 20:53:02 +0300

    rsi ping alert fixed minor issues

--- fused_home/rsi_ping_alert.py
+++ fused_home/rsi_ping_alert.py
@@ -1,7 +1,7 @@
 from os.path import expanduser
 from time import time, sleep
 
-from fused_home.irssinotifier import IrssiNotifier
+from irssinotifier import IrssiNotifier
 
 
 with open(expanduser("~/.irssinotifier.key")) as f:
@@ -10,7 +10,7 @@ with open(expanduser("~/.irssinotifier.key")) as f:
 
 notifier = IrssiNotifier(API_KEY, PASSWORD)
 while True:
-    with open('~/domains/my.kix.co.il/rpi-last.txt', 'r') as f:
+    with open(expanduser('~phantom-web/domains/my.kix.co.il/rpi-last.txt'), 'r') as f:
         try:
             timestamp = float(f.read().strip())
             delta = time() - timestamp
