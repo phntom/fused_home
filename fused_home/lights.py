1b2518fd097243736baa4e37599ed8c9d23b2a16
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-02-02 21:15:25 +0200

    added timestamps to lights

--- fused_home/lights.py
+++ fused_home/lights.py
@@ -2,29 +2,37 @@
 # -*- coding: utf-8 -*-
 import logging
 import sqlite3
+from datetime import date, datetime
+from time import time
 from os.path import expanduser
 
 import yeelight
 
 logger = logging.getLogger(__name__)
 
-TABLE_COLUMNS = ['ip', 'port', 'id', 'model', 'fw_ver', 'support', 'power', 'bright', 'color_mode', 'ct', 'rgb',
-                 'hue', 'sat', 'name']
+CAP_COLUMNS = ['id', 'model', 'fw_ver', 'support', 'power', 'bright', 'color_mode', 'ct', 'rgb', 'hue', 'sat', 'name']
+TABLE_COLUMNS = ['date', 'time', 'timestamp', 'ip', 'port'] + CAP_COLUMNS
 
 
 def main(c):
-    global TABLE_COLUMNS
+    global TABLE_COLUMNS, CAP_COLUMNS
     while True:
         for bulb in yeelight.discover_bulbs(timeout=5):
             ip, port, caps = bulb['ip'], bulb['port'], bulb['capabilities']
-            caps['ip'] = ip
-            caps['port'] = port
-            c.execute('SELECT * FROM lights WHERE ip = ? AND port = ? ORDER BY rowid DESC LIMIT 1', (ip, port))
+            cols = ', '.join(CAP_COLUMNS)
+            c.execute(f'SELECT {cols} FROM lights WHERE ip = ? AND port = ? ORDER BY rowid DESC LIMIT 1', (ip, port))
             last_entry = c.fetchone()
             if last_entry is not None:
                 last_entry = dict(zip(TABLE_COLUMNS, last_entry))
                 if last_entry == caps:
                     continue
+            caps.update({
+                'ip': ip,
+                'port': port,
+                'date': date.today().strftime("%Y-%m-%d"),
+                'time': datetime.now().strftime('%H:%M:%S'),
+                'timestamp': int(time()),
+            })
             keys = ','.join(caps.keys())
             questions = ','.join(['?'] * len(caps))
             c.execute(f'INSERT INTO lights({keys}) VALUES ({questions})', tuple(caps.values()))
