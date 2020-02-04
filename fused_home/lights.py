6bdc69263428e3989d3f984e408ca85d128c70fc
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-02-04 08:32:16 +0200

    cache for lights

--- fused_home/lights.py
+++ fused_home/lights.py
@@ -14,21 +14,33 @@ logger = logging.getLogger(__name__)
 INTERFACE = environ.get('EXTERNAL_INTERFACE', False)
 CAP_COLUMNS = ['id', 'model', 'fw_ver', 'support', 'power', 'bright', 'color_mode', 'ct', 'rgb', 'hue', 'sat', 'name']
 TABLE_COLUMNS = ['date', 'time', 'timestamp', 'ip', 'port'] + CAP_COLUMNS
+CACHE = {}
+
+
+def fetch_light(ip, port, conn):
+    global CACHE
+    key = (ip, port)
+    entry = CACHE.get(key)
+    if entry is None:
+        cursor = conn.cursor()
+        cols = ', '.join(CAP_COLUMNS)
+        cursor.execute(f'SELECT {cols} FROM lights WHERE ip = ? AND port = ? ORDER BY rowid DESC LIMIT 1', key)
+        entry = cursor.fetchone()
+        CACHE[key] = entry
+    return entry
 
 
 def main(conn):
-    global TABLE_COLUMNS, CAP_COLUMNS, INTERFACE
     while True:
         for bulb in yeelight.discover_bulbs(timeout=5, interface=INTERFACE):
             cursor = conn.cursor()
             ip, port, caps = bulb['ip'], bulb['port'], bulb['capabilities']
-            cols = ', '.join(CAP_COLUMNS)
-            cursor.execute(f'SELECT {cols} FROM lights WHERE ip = ? AND port = ? ORDER BY rowid DESC LIMIT 1', (ip, port))
-            last_entry = cursor.fetchone()
+            last_entry = fetch_light(ip, port, conn)
             if last_entry is not None:
                 last_entry = dict(zip(TABLE_COLUMNS, last_entry))
                 if last_entry == caps:
                     continue
+            CACHE[(ip, port)] = caps
             caps.update({
                 'ip': ip,
                 'port': port,
