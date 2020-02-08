59ac9c73b26dbe11d759c9e2f01c815ae5993439
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-02-08 10:26:08 +0200

    now tracking disconnected lights

--- fused_home/lights.py
+++ fused_home/lights.py
@@ -12,34 +12,38 @@ import yeelight
 logger = logging.getLogger(__name__)
 
 INTERFACE = environ.get('EXTERNAL_INTERFACE', False)
-CAP_COLUMNS = ['id', 'model', 'fw_ver', 'support', 'power', 'bright', 'color_mode', 'ct', 'rgb', 'hue', 'sat', 'name']
-TABLE_COLUMNS = ['date', 'time', 'timestamp', 'ip', 'port'] + CAP_COLUMNS
+LIGHTS_CAP_COLUMNS = ['id', 'model', 'fw_ver', 'support', 'power', 'bright', 'color_mode', 'ct', 'rgb', 'hue', 'sat', 'name']
+META_COLUMNS = ['date', 'time', 'timestamp', 'ip', 'port']
 CACHE = {}
 
 
-def fetch_light(ip, port, conn):
+def fetch_light(ip, port, cursor):
     global CACHE
     key = (ip, port)
     entry = CACHE.get(key)
     if entry is None:
-        cursor = conn.cursor()
-        cols = ', '.join(CAP_COLUMNS)
+        cols = ', '.join(LIGHTS_CAP_COLUMNS)
         cursor.execute(f'SELECT {cols} FROM lights WHERE ip = ? AND port = ? ORDER BY rowid DESC LIMIT 1', key)
         entry = cursor.fetchone()
+        if entry is None:
+            return None
+        entry = dict(zip(LIGHTS_CAP_COLUMNS, entry))
         CACHE[key] = entry
     return entry
 
 
-def main(conn):
+def main():
+    seen = set(CACHE.keys())
     while True:
-        for bulb in yeelight.discover_bulbs(timeout=5, interface=INTERFACE):
-            cursor = conn.cursor()
+        seen.clear()
+        cursor = conn.cursor()
+        for bulb in yeelight.discover_bulbs(timeout=15, interface=INTERFACE):
             ip, port, caps = bulb['ip'], bulb['port'], bulb['capabilities']
-            last_entry = fetch_light(ip, port, conn)
-            if last_entry is not None:
-                last_entry = dict(zip(TABLE_COLUMNS, last_entry))
-                if last_entry == caps:
-                    continue
+            if (ip, port) in seen:
+                seen.remove((ip, port))
+            last_entry = fetch_light(ip, port, cursor)
+            if last_entry is not None and last_entry == caps:
+                continue
             CACHE[(ip, port)] = caps.copy()
             caps.update({
                 'ip': ip,
@@ -51,6 +55,19 @@ def main(conn):
             keys = ','.join(caps.keys())
             questions = ','.join(['?'] * len(caps))
             cursor.execute(f'INSERT INTO lights({keys}) VALUES ({questions})', tuple(caps.values()))
+        for ip, port in seen:
+            last_entry = CACHE.pop((ip, port))
+            last_entry.update({
+                'ip': ip,
+                'port': port,
+                'date': date.today().strftime("%Y-%m-%d"),
+                'time': datetime.now().strftime('%H:%M:%S'),
+                'timestamp': int(time()),
+                'power': 'disconnected',
+            })
+            keys = ','.join(last_entry.keys())
+            questions = ','.join(['?'] * len(last_entry))
+            cursor.execute(f'INSERT INTO lights({keys}) VALUES ({questions})', tuple(last_entry.values()))
         conn.commit()
 
 
@@ -63,7 +80,8 @@ if __name__ == "__main__":
                         )
     logger.info(f"started lights monitor, using interface {INTERFACE}")
     conn = sqlite3.connect(expanduser('~/logs/lights.db'))
-    columns = ",".join(TABLE_COLUMNS)
-    conn.cursor().execute(f"""CREATE TABLE IF NOT EXISTS lights ({columns})""")
+
+    conn.cursor().execute(f"""CREATE TABLE IF NOT EXISTS lights ({",".join(META_COLUMNS + LIGHTS_CAP_COLUMNS)})""")
     conn.cursor().execute(f"""CREATE INDEX IF NOT EXISTS ip_port ON lights(ip, port)""")
-    main(conn)
+
+    main()
