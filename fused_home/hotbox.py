46c40664fe5d3c10e8110206a5c09daea9e5a740
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-04-15 14:35:49 +0300

    hotbox login after restart; run.sh doesn't die on process exit

--- fused_home/hotbox.py
+++ fused_home/hotbox.py
@@ -29,23 +29,7 @@ with open(expanduser(LOG_FILE)) as f:
     ROUTER_KEY = json.load(f)
 MODEM_PASSWORD = ROUTER_KEY.pop('MODEM_PASSWORD')
 
-res = requests.get('http://192.168.100.1/reseau-pa3-frequencecable.html')
-res.raise_for_status()
 session_key = None
-if '>location.href="/login.html"<' in res.text:
-    logging.info("logging in to session")
-    # login
-    res = requests.get('http://192.168.100.1/login.html')
-    res.raise_for_status()
-    session_key = session_key_matcher.match(res.text).group(1)
-    logging.info("session key is {}".format(session_key))
-    res = requests.post(f'http://192.168.100.1/postlogin.cgi?sessionKey={session_key}', data={
-        'sessionKey': session_key,
-        'loginUsername': MODEM_USER,
-        'loginPassword': MODEM_PASSWORD,
-    })
-    if 'var RefreshPage = ' not in res.text:
-        raise ValueError('login failed')
 
 while True:
     if (datetime.now() - last_heartbeat).total_seconds() > HEARTBEAT_SECONDS:
@@ -54,8 +38,30 @@ while True:
 
     res = requests.get('http://192.168.100.1/reseau-pa3-frequencecable.html')
     res.raise_for_status()
+
+    if 'document.write(AccessMemberLimit);' in res.text:
+        logging.warning("Permission denied, another user is already logged; Sleeping 120 seconds")
+        sleep(120)
+        continue
+
+    if '>location.href="/login.html"<' in res.text:
+        logging.info("logging in to session")
+        # login
+        res = requests.get('http://192.168.100.1/login.html')
+        res.raise_for_status()
+        session_key = session_key_matcher.match(res.text).group(1)
+        logging.info("session key is {}".format(session_key))
+        res = requests.post(f'http://192.168.100.1/postlogin.cgi?sessionKey={session_key}', data={
+            'sessionKey': session_key,
+            'loginUsername': MODEM_USER,
+            'loginPassword': MODEM_PASSWORD,
+        })
+        if 'var RefreshPage = ' not in res.text:
+            raise ValueError('login failed')
+        continue
+
     new_session_key = session_key_matcher.match(res.text).group(1)
-    if not session_key:
+    if new_session_key and not session_key:
         session_key = new_session_key
         logging.info("session key is {}".format(session_key))
 
@@ -74,11 +80,12 @@ while True:
         logging.warning("restarting cable modem")
 
         res = requests.post(f'http://192.168.100.1/reseau-pa3-frequencecable.cgi?sessionKey={session_key}', data={
-            'sessionKey': session_key,
             'CmStartupDsFreq': '999999',
+            'sessionKey': session_key,
             'action': '1',
         })
         res.raise_for_status()
+        logging.debug(res.text)
 
         logging.warning("waiting 15 seconds")
         sleep(15)
--- run.sh
+++ run.sh
@@ -9,10 +9,6 @@ python3 fused_home/sensors.py &
 python3 fused_home/hotbox.py &
 
 while true; do
-    ps aux | grep -q boiler_watchdog.py || exit 1
-    ps aux | grep -q ddns.py || exit 2
-    ps aux | grep -q lights.py || exit 3
-    ps aux | grep -q sensors.py || exit 4
-    ps aux | grep -q hotbox.py || exit 5
     sleep 60
+    [ `pgrep python3 | wc -l` -eq 5 ] || exit 1
 done
