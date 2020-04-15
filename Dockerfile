4d1e6baf8b9299597550c6be89b0c815e5999cdd
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-04-15 14:08:58 +0300

    broadlink broke something, upgraded packages and downgraded broadlink

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
-FROM python:3.6-alpine
+FROM python:3.7-alpine
 RUN apk add --no-cache gcc g++ make libffi-dev openssl-dev
 COPY requirements.txt .
 RUN pip install -r requirements.txt
 COPY . .
-RUN chmod +x run.sh
 CMD ["./run.sh"]
--- run.sh
+++ run.sh
@@ -5,10 +5,14 @@ export EXTERNAL_INTERFACE=eth1
 python3 fused_home/boiler_watchdog.py &
 python3 fused_home/ddns.py &
 python3 fused_home/lights.py &
+python3 fused_home/sensors.py &
+python3 fused_home/hotbox.py &
 
 while true; do
     ps aux | grep -q boiler_watchdog.py || exit 1
     ps aux | grep -q ddns.py || exit 2
     ps aux | grep -q lights.py || exit 3
+    ps aux | grep -q sensors.py || exit 4
+    ps aux | grep -q hotbox.py || exit 5
     sleep 60
 done
