6adc62f22b3de55d040e91375973449831f8d7a2
Anton Wolkov <7001904+phntom@users.noreply.github.com>
2020-08-14 13:56:20 +0300

    add bandwidth hogs to alternate route

--- fused_home/router.py
+++ fused_home/router.py
@@ -1,17 +1,10 @@
+from time import sleep
+
 import netaddr as netaddr
 from netaddr import IPNetwork
 from routeros_api import RouterOsApiPool
 
-from common import get_config, setup_logging
-
-setup_logging('routes.log')
-CONFIG = get_config('router', 'modem')
-ROUTER_KEY = CONFIG['ROUTER_CONFIG']
-MODEM_PASSWORD = CONFIG['MODEM_PASSWORD']
-
-router_api = RouterOsApiPool(**ROUTER_KEY).get_api()
-
-route = router_api.get_resource('/ip/route')
+from common import get_config, setup_logging, heartbeat
 
 ROUTE_DISTANCE = 39
 
@@ -35,44 +28,85 @@ hot_lists = {
     'com.linode.speedtest',
 }
 
-set_routes = set()
 
-for rrr in route.get(distance=str(ROUTE_DISTANCE)):
-    set_routes.add(str(rrr['dst-address']))
+def load_existing_routes(route, set_routes):
+    for rrr in route.get(distance=str(ROUTE_DISTANCE)):
+        set_routes.add(str(rrr['dst-address']))
 
-add_routes = set()
 
-for addr in router_api.get_resource('/ip/firewall/address-list').get():
-    if addr['list'] in hot_lists:
-        rng = addr['address'] + '/32'  # '.'.join(addr['address'].split('.')[0:2]) + '.0.0/16'
-        if rng in set_routes:
+def load_address_list(router_api, set_routes, add_routes):
+    for addr in router_api.get_resource('/ip/firewall/address-list').get():
+        if addr['list'] in hot_lists:
+            rng = addr['address'] + '/32'  # '.'.join(addr['address'].split('.')[0:2]) + '.0.0/16'
+            if rng in set_routes:
+                continue
+            add_routes.add(rng)
+    add_routes = add_routes.union(set_routes)
+    new_routes = {str(x) for x in netaddr.cidr_merge([IPNetwork(x) for x in add_routes])}
+    remove_routes = set_routes - new_routes
+    new_routes -= set_routes
+    return new_routes, remove_routes
+
+
+def add_new_routes(route, new_routes, remove_routes):
+    for rng in new_routes:
+        print(f"adding route {rng}")
+        route.add(gateway='100.96.40.1',
+                  distance=str(ROUTE_DISTANCE),
+                  scope='30',
+                  **{
+                      'target-scope': '10',
+                      'dst-address': str(rng),
+                      'check-gateway': 'ping',
+                  })
+    for rng in remove_routes:
+        old_id = route.get(**{'dst-address': rng})[0]['id']
+        print(f"removing route {rng} {old_id}")
+        route.remove(id=old_id)
+
+
+def add_fast_routes(add_routes, connections):
+    for connection in connections.get():
+        if connection['protocol'] != 'tcp' or connection['tcp-state'] != 'established':
             continue
-        add_routes.add(rng)
+        orig_rate = int(connection['orig-rate'])
+        repl_rate = int(connection['repl-rate'])
+        rng = connection['dst-address'].split(':')[0] + '/32'
+        if rng in add_routes:
+            continue
+        if repl_rate > 8192000:
+            print('download speed {}mb for connection {} --> {}'.format(
+                connection['dst-address'], connection['src-address'], repl_rate/8/1024/1024)
+            )
+            add_routes.add(rng)
+        if orig_rate > 8192000:
+            print('upload speed {}mb for connection {} --> {}'.format(
+                connection['dst-address'], connection['src-address'], repl_rate/8/1024/1024)
+            )
+            add_routes.add(rng)
 
-new_routes = {str(x) for x in
-              netaddr.cidr_merge([IPNetwork(x) for x in
-                                  add_routes.union(set_routes)])}
-new_routes -= set_routes
 
-if not new_routes:
-    exit(0)
+def main():
+    set_routes = set()
+    add_routes = set()
+    setup_logging('routes.log')
+    config = get_config('router')
+    router_key = config['ROUTER_CONFIG']
+    router_api = RouterOsApiPool(**router_key).get_api()
+    route = router_api.get_resource('/ip/route')
+    load_existing_routes(route, set_routes)
+    connections = router_api.get_resource('/ip/firewall/connection')
+    while True:
+        heartbeat()
+        add_routes.clear()
+        add_fast_routes(add_routes, connections)
+        new_routes, remove_routes = load_address_list(router_api, set_routes, add_routes)
+        if new_routes:
+            add_new_routes(route, new_routes, remove_routes)
+            set_routes -= remove_routes
+            set_routes = set_routes.union(new_routes)
+        sleep(30)
 
-for rng in new_routes:
-    print(f"adding route {rng}")
-    route.add(gateway='100.96.40.1',
-              distance=str(ROUTE_DISTANCE),
-              scope='30',
-              **{
-                  'target-scope': '10',
-                  'dst-address': str(rng),
-                  'check-gateway': 'ping',
-              })
 
-set_routes = {str(rrr['dst-address']) for rrr in route.get(distance=str(ROUTE_DISTANCE))}
-minimal_set = {str(x) for x in
-               netaddr.cidr_merge([IPNetwork(x) for x in set_routes])}
-set_routes -= minimal_set
-for rng in set_routes:
-    old_id = route.get(**{'dst-address': rng})[0]['id']
-    print(f"removing route {rng} {old_id}")
-    route.remove(id=old_id)
+if __name__ == '__main__':
+    main()
